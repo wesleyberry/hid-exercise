94afbd631c8e4720a787fa7ec40788c1b6672d08
Wesley Berry <wesleyberry52@gmail.com>
2019-11-05 13:27:24 -0600

    server boiler plate...

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
