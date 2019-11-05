db142cdac6a7130a5af737e8110110c373549be6
Wesley Berry <wesleyberry52@gmail.com>
2019-11-05 14:19:06 -0600

    set up fonts and header

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
+import Foundation from 'react-foundation';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Navbar />
+        <Route exact path="/" />
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+export default function Navbar() {
+    return (
+        <h1 className="mainHeader">Squirrel Whisperers</h1>
+        
+    );
+}
