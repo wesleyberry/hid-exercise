d389064cd147e7544e6fa879938cb0b96447949e
Wesley Berry <wesleyberry52@gmail.com>
2019-11-05 23:35:48 -0600

    added header image

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import imageHeader from '../../Images/Squirrel_Whisperers.png';
 
 export default function Navbar() {
     return (
         <React.Fragment>
-            <h1 className="mainHeader">Squirrel Whisperers</h1>
+            <div className="container headerContainer">
+                <img src={ imageHeader } alt="Squirrel Whisperers" id="headerImage"/>
+            </div>
             <nav className="navbar navbar-expand-md navbar-light">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
